ca5bd73209af8d367fb62c1abc58763b97e2e97e
Sean Toner <stoner@localhost.localdomain>
2016-12-20 16:45:09 -0500

    Setting polarize.properties file rather than setting env

--- src/Command/scratch_jenkins.purs
+++ src/Command/scratch_jenkins.purs
@@ -1,14 +1,21 @@
 module Command.Starter where
 
 import Prelude
-import Control.Monad.Eff (Eff)
 import Node.Buffer as Buffer
-import Node.Buffer (BUFFER)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import DOM.Node.Node (lookupNamespaceURI)
+import Data.Either (Either(..))
 import Data.Foldable (foldl)
+import Data.Foreign.Keys (keys)
+import Data.Map (Map, fromFoldable)
 import Data.Maybe (Maybe(..))
-import Node.ChildProcess (ChildProcess, CHILD_PROCESS, SpawnOptions, defaultSpawnOptions, Exit(..), spawn, onExit, stdout)
+import Data.String.Regex (Regex, replace)
+import Data.String.Regex.Flags (noFlags)
+import Data.Tuple (Tuple(..))
+import Node.Buffer (BUFFER)
+import Node.ChildProcess (CHILD_PROCESS, SpawnOptions, defaultSpawnOptions, Exit(..), spawn, onExit, stdout)
 import Node.Encoding (Encoding(UTF8))
 import Node.Process (lookupEnv, PROCESS)
 import Node.Stream (onData)
@@ -21,6 +28,12 @@ type ProcEff = forall e. Eff ( process :: PROCESS
                              , err :: EXCEPTION
                              , buffer :: BUFFER | e
                              ) Unit
+type EffProc = forall e. Eff ( process :: PROCESS
+                             , cp :: CHILD_PROCESS
+                             , console :: CONSOLE
+                             , err :: EXCEPTION
+                             , buffer :: BUFFER | e
+                             ) String
 
 -- | First arg is default if second arg is Nothing
 default' :: String -> Maybe String -> String
@@ -36,6 +49,7 @@ launch cmd args opts = do
   let defaultExitHdlr exit = case exit of
         Normally x -> log $ "Got exit code of: " <> show x
         _ -> log $ "Exited due to signal: " <> show exit
+      -- TODO: explain what this function does
       defaultDataHdlr cp = onData (stdout cp) (Buffer.toString UTF8 >=> log)
   cmd' <- spawn cmd args opts
   onExit cmd' defaultExitHdlr
@@ -44,14 +58,14 @@ launch cmd args opts = do
 
 -- | gets the pub and pvt keys
 -- FIXME: replace this with a regular http GET instead of shelling out to wget
-get_auto_key :: String -> String -> CPEffect
-get_auto_key keyname output = do
+getAutoKey :: String -> String -> CPEffect
+getAutoKey keyname output = do
   let args = ["-nv", "http://auto-services.usersys.redhat.com/rhsm/keys/" <> keyname, "-O", output]
   launch "wget" args defaultSpawnOptions
 
 -- | Does a git checkout in the current working directory
-git_checkout :: Array String -> CPEffect
-git_checkout args = do
+gitCheckout :: Array String -> CPEffect
+gitCheckout args = do
   launch "git" args defaultSpawnOptions
 
 -- | Remove reults from a prior run and avoid failed jobs due to missing result files
@@ -65,7 +79,6 @@ cleanup = do
   launch "touch" ["test-output/registration_report.html", "test-output/hw_info_dump.tar"] defaultSpawnOptions
   log "end of cleanup"
 
-
 -- | compile with lein
 compile :: CPEffect
 compile = do
@@ -74,46 +87,23 @@ compile = do
   launch "lein" ["compile"] defaultSpawnOptions
   log "end of lein compile"
 
-{- Make a function set_env_vars which injects these variables into the global env
+getClasspath :: Eff (cp :: CHILD_PROCESS, console :: CONSOLE, err :: EXCEPTION, buffer :: BUFFER | e) String
+getClasspath = do
+  launch "lein" ["classpath"] defaultSpawnOptions
 
-POLARIZE_VERSION=0.5.4-SNAPSHOT
-PROJECT_ID=RHEL6
-CURRENT_XUNIT=`echo "${JENKINS_URL}view/Scratch/job/${JOB_NAME}/${BUILD_NUMBER}/artifact/test-output/testng-polarion.xml" | sed 's/http/https/g'`
-PLANNEDIN_MILESTONE="6.9 Pre-testing"
-TEMPLATE_ID="sean toner master template test"
-TESTRUN_TITLE="Polarize testing"
-PROJECT_NAME="RHSM "
-NEW_XUNIT="/tmp/modified-testng-polarion.xml"    # Where to write  a temporary xunit xml based off of CURRENT_XUNIT
--}
-
-{- Make a function create_properties which writes out all of this to the test-output/polarize.properties file
-create_properties :: String -> Array String -> CPEffect
-create_properties fpath lines = map (\fpath line -> writeTo fpath line) lines
-
-create_properties "test-output/polarize.properties" lines
-##########################################################
-# Write out the Project and path to artifact
-# Take care to wrap anything with space in it with \"${THING_WITH_SPACE}\"
-##########################################################
-echo "POLARIZE_VERSION=${POLARIZE_VERSION}" >> test-output/polarize.properties
-echo "PROJECT_ID=${PROJECT_ID}" >> test-output/polarize.properties
-echo "CURRENT_XUNIT=${CURRENT_XUNIT}" >> test-output/polarize.properties
-echo "PLANNED_IN_MILESTONE=\"${PLANNEDIN_MILESTONE}\"" >> test-output/polarize.properties
-echo "TEMPLATE_ID=\"${TEMPLATE_ID}\"" >> test-output/polarize.properties
-echo "TESTRUN_TITLE=\"${TESTRUN_TITLE}\"" >> test-output/polarize.properties
-echo "PROJECT_NAME=\"${PROJECT_NAME}" >> test-output/polarize.properties
-echo "NEW_XUNIT=${NEW_XUNIT}" >> test-output/polarize.properties
-echo "JENKINSJOBS=${BUILD_URL}" >> test-output/polarize.properties
-echo "NOTES=${BUILD_URL}TestNG_Report" >> test-output/polarize.properties
--}
 
 -- | The main script that kicks everything off
 main :: ProcEff
 main = do
+  -- This just feels really ugly.  I should be able to put all these strings into a map.
   quick_build <- lookupEnv "QUICK_BUILD"
   auto_branch <- lookupEnv "AUTOMATION_BRANCH"
   server_branch <- lookupEnv "SERVER_BRANCH"
   rpm_urls <- lookupEnv "RPM_URLS"
+  jenkins_url <- lookupEnv "JENKINS_URL"
+  build_number <- lookupEnv "BUILD_NUMBER"
+  job_name <- lookupEnv "JOB_NAME"
+  c_xunit <- lookupEnv "CURRENT_XUNIT"
   let auto_branch' = case auto_branch of
                        (Just ab) -> ab
                        _ -> ""
@@ -124,17 +114,38 @@ main = do
                     then ""
                     else default' "" rpm_urls
       git_args = ["checkout", "upstream/" <> (default' "master" auto_branch)]
+      default = jenkins_url <> "view/Scratch/job/" <> job_name <> "/" <>  build_number <>
+                "/artifact/test-output/testng-polarion.xml"
+      current_xunit = case c_xunit of
+                        Nothing ->  default
+                        (Just "") -> default
+                        (Just x) -> x
+      re = Regex """^https"""
+      current' = replace re "http" current_xunit
 
   log $ "server_branch = " <> server_branch'
   log $ foldl (\acc n -> acc <> n <> ",") "git_args = [" git_args <> "]"
+  log $ "current' = " <> current'
 
   -- Do the checkout, grab the keys, cleanup and compile
-  git_checkout git_args
-  get_auto_key "rhsm-qe.pub" ".ssh/rhsm-qe.pub"
-  get_auto_key "rhsm-qe" ".ssh/rhsm-qe"
+  gitCheckout git_args
+  getAutoKey "rhsm-qe.pub" ".ssh/rhsm-qe.pub"
+  getAutoKey "rhsm-qe" ".ssh/rhsm-qe"
   cleanup
   compile
-  -- set_env_vars
-  -- create_properties
+
+  -- Set all the JAVA_ARGS
+
+  -- Kick off the test
+  -- java -cp "`lein classpath`" "${JAVAARGS[@]}" org.testng.TestNG "${TESTNGARGS[@]}"  $TEST_SUITES || true
+
+
+  {- There will be a file called polarize.properties that exists in $WORKSPACE.  If CURRENT_XUNIT from the job has no
+  value, use the default.  Then write the following into the polarize.properties file.
+
+  echo "CURRENT_XUNIT=${CURRENT_XUNIT}" >> polarize.properties
+  echo "JENKINSJOBS=${BUILD_URL}" >> polarize.properties
+  echo "NOTES=${BUILD_URL}TestNG_Report" >> polarize.properties
+  -}
 
   log "Done"
