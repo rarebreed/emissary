751dd50b2369c29d310520c9cd2611ffc128df9d
Sean Toner <placeoftheway@gmail.com>
2017-01-23 07:01:05 -0500

    Got makeAff to work

--- src/Command/Command.purs
+++ src/Command/Command.purs
@@ -11,10 +11,14 @@ module Command where
 
 import Control.Monad.Aff
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Exception (EXCEPTION, Error, error, message)
 import Control.Monad.Eff.Ref (Ref, REF, newRef, writeRef, readRef, modifyRef)
+
 import Data.Maybe (Maybe(..))
+import Data.Either (Either(..))
+
 import Node.Buffer (Buffer, BUFFER, toString)
 import Node.ChildProcess (SpawnOptions, defaultSpawnOptions, spawn, ChildProcess, CHILD_PROCESS, onExit, Exit(..), stdout)
 import Node.Encoding (Encoding(..))
@@ -43,47 +47,52 @@ onDataSave ref buff = do
   pure unit
 
 
-type CmdEff = forall e. Eff ( cp :: CHILD_PROCESS
-                            , ref :: REF
-                            , err :: EXCEPTION
-                            , buffer :: BUFFER
-                            , console :: CONSOLE
-                            | e
-                            )
-                            Unit
-type SimpleCallback = forall a. a -> CmdEff
+type CmdEff e = ( cp :: CHILD_PROCESS
+                , ref :: REF
+                , err :: EXCEPTION
+                , buffer :: BUFFER
+                , console :: CONSOLE
+                | e
+                )
 
--- | Launch a child process along with a callback
-launch :: forall a
+
+getOutput :: forall e. Command -> Eff (ref :: REF, console :: CONSOLE | e) Unit
+getOutput (Command {output}) = do
+  out <- readRef output
+  log out
+
+
+-- | Launch a child process along with an error callback and success callback
+launchImpl :: forall e
+            . Command
+           -> (Error -> Eff (CmdEff e) Unit)
+           -> (Command -> Eff (CmdEff e) Unit)
+           -> Eff (CmdEff e) Unit
+launchImpl cmd@(Command {command, args, opts, process, output}) ecb scb = do
+  cp <- spawn command args opts
+  writeRef process (Just cp)
+  -- The output is accumulated to cmd.output each time the data event is caught
+  onData (stdout cp) (onDataSave output)
+  -- On finish, call
+  onExit cp \exit -> case exit of
+              Normally 0 -> (scb cmd)
+              Normally x -> do
+                let err = "command failed with ret code: " <> show x
+                ecb $ error err
+              BySignal _ -> do
+                let err = "command failed due to signal: " <> (show exit)
+                ecb $ error err
+
+defErr :: forall e. Error -> Eff (console :: CONSOLE | e) Unit
+defErr err = log $ message err
+
+-- asynchronous launch
+launch :: forall e
         . Command
-       -> (a -> CmdEff)
-       -> a
-       -> CmdEff
-launch (Command {command, args, opts, process, output}) cb a = do
-  cp <- spawn command args opts
-  writeRef process (Just cp)
-  -- The output is accumulated to cmd.output each time the data event is caught
-  onData (stdout cp) (onDataSave output)
-  -- On success, call
-  onExit cp \exit -> case exit of
-              Normally 0 -> cb a
-              Normally x -> log $ "command failed with ret code: " <> show x
-              BySignal _ -> log $ "command failed due to signal: " <> (show exit)
+       -> Aff (CmdEff e) Command
+launch cmd  =  makeAff $ launchImpl cmd
 
-
-launch' :: Command -> CmdEff
-launch' (Command {command, args, opts, process, output}) = do
-  cp <- spawn command args opts
-  writeRef process (Just cp)
-  -- The output is accumulated to cmd.output each time the data event is caught
-  onData (stdout cp) (onDataSave output)
-  -- On success, call
-  onExit cp \exit -> case exit of
-              Normally 0 -> log "Program exited with ret code 0"
-              Normally x -> log $ "command failed with ret code: " <> (show x)
-              BySignal _ -> log $ "command failed due to signal: " <> (show exit)
-
-
+test :: forall e. Eff (CmdEff e) Unit
 test = do
   lastOutput <- newRef ""
   lastProc <- newRef Nothing
@@ -105,12 +114,26 @@ test = do
                           , output: sndOutput
                           , process: sndProc
                           }
+      nameCmd = Command { command: "python"
+                     , args: ["-u", """C:\projects\dummy.py"""]
+                     , opts: defaultSpawnOptions
+                     , combineErr: true
+                     , output: firstOutput
+                     , process: firstProc
+                     }
       firstCmd = Command { command: "python"
-                 , args: ["-u", "/home/stoner/dumdum.py"]
+                 , args: ["-u", """C:\projects\dumdum.py"""]
                  , opts: defaultSpawnOptions
                  , combineErr: true
                  , output: firstOutput
                  , process: firstProc
                  }
-  launch firstCmd launch' lastCmd
-  log $ "Final output of first command:\n" <> output
+      runcmds = do
+        pyproc <- launch firstCmd
+        pyproc2 <- launch nameCmd
+        pure pyproc2
+
+  runAff defErr getOutput runcmds
+
+  -- pyOut <- getOutput pyProc
+  liftEff $ log "done"
